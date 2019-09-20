dd1fba4cad59e8598268aedcceae79b81168f96a
Iqbal Novramadani <iqbal.slf@gmail.com>
2019-09-21 00:13:07 +0700

    change prod.keys

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,17 @@
 // keys.js - figure out what set of credentials to return
 if (process.env.NODE_ENV === 'production') {
   // we are in production - return the prod set of keys
-  module.exports = require('./prod');
+  // module.exports = require('./prod');
+  module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY,
+    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+    sendGridKey: process.env.SEND_GRID_KEY,
+    redirectDomain: process.env.REDIRECT_DOMAIN
+  };
 } else {
   // we are in development - return the dev keys!!!
   module.exports = require('./dev');
