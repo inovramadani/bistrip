6a3cbcefe1888d42767939c10146c5ec74ec6225
Iqbal Novramadani <iqbal.slf@gmail.com>
2019-09-21 00:36:41 +0700

    console.log mongoURI

--- services/mongoUtil.js
+++ services/mongoUtil.js
@@ -2,11 +2,12 @@ const mongodb = require('mongodb')
 const keys = require('../config/keys')
 const mongoClient = mongodb.MongoClient
 
-const dbName = 'masjeed'
+const dbName = 'flowngin-dev'
 var _db
 
 module.exports = {
   connectToDB: (callback) => {
+    console.log('mongoURI: ', keys.mongoURI)
     mongoClient.connect(keys.mongoURI, { useNewUrlParser: true }, (err, client) => {
     	_db = client.db(dbName)
     	return callback(err)
